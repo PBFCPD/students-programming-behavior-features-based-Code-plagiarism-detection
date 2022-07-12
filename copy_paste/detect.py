2d5b9896e675acf2a6eef74065561535efe4e130
anzyt123 <48685573+anzyt123@users.noreply.github.com>
2022-07-12 14:43:47 +0800

    Update detect.py

--- copy_paste/detect.py
+++ copy_paste/detect.py
@@ -1,7 +1,7 @@
 import MySQLdb
 
-db1 = MySQLdb.connect("202.4.155.100", "root", "buctacm4726", "record", charset='utf8')
-db2 = MySQLdb.connect("202.4.155.100", "root", "buctacm4726", "jol", charset='utf8')
+db1 = MySQLdb.connect("202.4.155.100", "root", "*", "*", charset='utf8')
+db2 = MySQLdb.connect("202.4.155.100", "root", "*", "*", charset='utf8')
 
 cursor1 = db1.cursor()
 cursor2 = db2.cursor()
