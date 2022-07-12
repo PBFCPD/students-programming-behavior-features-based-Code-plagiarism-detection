19eefa8ba419fdc364be553d22cd36e04e38d701
anzyt123 <48685573+anzyt123@users.noreply.github.com>
2022-07-12 14:42:55 +0800

    Update data.py

--- copy_paste/data.py
+++ copy_paste/data.py
@@ -3,8 +3,8 @@ import pandas as pd
 from pandas import DataFrame,read_excel
 from numpy import *
 
-# db1 = MySQLdb.connect("202.4.155.100", "root", "buctacm4726", "record", charset='utf8')
-# db2 = MySQLdb.connect("202.4.155.100", "root", "buctacm4726", "jol", charset='utf8')
+# db1 = MySQLdb.connect("202.4.155.100", "root", "*", "*", charset='utf8')
+# db2 = MySQLdb.connect("202.4.155.100", "root", "*", "*", charset='utf8')
 #
 # cursor1 = db1.cursor()
 # cursor2 = db2.cursor()
